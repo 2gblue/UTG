2705f73aa88ade778655d4fe04c178c1d38c0955
Wei Min <107160038+2gblue@users.noreply.github.com>
2024-07-18 10:59:49 +0800

    temporary generate

--- pages/generate/index.vue
+++ pages/generate/index.vue
@@ -4,9 +4,11 @@
   <div class="container">
     <h2 class="middleTitle">Chosen Courses</h2>
     <el-divider></el-divider>
-    <el-table :data="courseData" stripe>
+    <el-table :data="selectedCourses" stripe>
       <el-table-column prop="courseCode" label="Course Code" width="200" />
       <el-table-column prop="courseName" label="Course Name" />
+      <el-table-column prop="exam" label="Exam" />
+      <el-table-column prop="credit" label="Credit Hour" />
     </el-table>
   </div>
   <br />
@@ -16,44 +18,56 @@
   <div class="container">
     <h2 class="middleTitle">Courses List</h2>
     <el-divider></el-divider>
-    <div class="search-bar">
-      <el-form :model="search" label-width="auto" style="width: 50%">
-        <el-input v-model="search.courseName" placeholder="Course Code/Name" />
-      </el-form>
-      <button><i class="bx bx-search-alt-2 bx-fw"></i></button>
-      <el-select v-model="search.exam" placeholder="Exam" style="width: 15%">
-        <el-option label="Yes" value="Y" />
-        <el-option label="No" value="N" />
-      </el-select>
-      <el-select
-        v-model="search.faculty"
-        placeholder="Faculty"
-        style="width: 30%"
+    <div>
+      <div class="search-bar">
+        <el-form :model="search" label-width="auto">
+          <el-input v-model="search.courseCode" placeholder="Course Code" />
+        </el-form>
+        <el-form :model="search" label-width="auto" style="width: 40%">
+          <el-input v-model="search.courseName" placeholder="Course Name" />
+        </el-form>
+        <el-select v-model="search.exam" placeholder="Exam" style="width: 15%">
+          <el-option label="All" value="*" />
+          <el-option label="Yes" value="Yes" />
+          <el-option label="No" value="No" />
+        </el-select>
+        <el-select
+          v-model="search.faculty"
+          placeholder="Faculty"
+          style="width: 30%"
+        >
+          <el-option
+            v-for="faculty in faculties"
+            :key="faculty.id"
+            :label="faculty.name"
+            :value="faculty.id"
+          />
+        </el-select>
+      </div>
+      <el-divider></el-divider>
+      <el-table
+        :data="coursesData"
+        stripe
+        @selection-change="handleSelectionChange"
+        row-key="id"
+        :reserve-selection="true"
       >
-        <el-option label="FK" value="FK" />
-        <el-option label="FTKMA" value="FTKMA" />
-      </el-select>
-    </div>
-    <el-divider></el-divider>
-    <el-table :data="courseData" stripe>
-      <el-table-column prop="courseCode" label="Course Code" />
-      <el-table-column prop="courseName" label="Course Name" width="400" />
-      <el-table-column prop="exam" label="Exam" />
-      <el-table-column prop="credit" label="Credit Hour" />
-      <el-table-column prop="faculty" label="Faculty" />
-      <el-table-column label="Actions">
-        <template #default="{ row }">
-          <el-checkbox v-model="row.checked" size="large" />
-        </template>
-      </el-table-column>
-    </el-table>
-    <div class="pageNavigation">
-      <el-pagination
-        :page-size="21"
-        :pager-count="3"
-        layout="prev, pager, next"
-        :total="100"
-      />
+        <el-table-column prop="courseCode" label="Course Code" />
+        <el-table-column prop="courseName" label="Course Name" />
+        <el-table-column prop="exam" label="Exam" width="100" />
+        <el-table-column prop="faculty" label="Faculty" />
+        <el-table-column prop="credit" label="Credit Hour" width="100" />
+        <el-table-column type="selection" width="55" />
+      </el-table>
+      <div class="pageNavigation">
+        <button @click="prevPage" :disabled="currentPage === 1">
+          Previous
+        </button>
+        <span>{{ currentPage }} / {{ totalPages }}</span>
+        <button @click="nextPage" :disabled="currentPage === totalPages">
+          Next
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -61,40 +75,125 @@
 <style scoped>
 @import url("../../assets/css/browse.css");
 @import url("../../node_modules/boxicons/css/boxicons.min.css");
+@import url("../../assets/css/pager.css");
 </style>
 
 <script setup>
-const search = ref({
+const client = useSupabaseClient();
+const coursesData = ref([]);
+const faculties = ref([]);
+const search = reactive({
+  courseCode: "",
   courseName: "",
   exam: "",
   faculty: "",
 });
-
-const courseData = [
-  {
-    courseCode: "BCS2143",
-    courseName: "Object Oriented Programming",
-    exam: "Y",
-    credit: "3",
-    faculty: "FACULTY OF COMPUTING",
-  },
-  {
-    courseCode: "BCS2233",
-    courseName: "Software Requirement Workshop",
-    exam: "N",
-    credit: "3",
-    faculty: "FACULTY OF COMPUTING",
-  },
-  {
-    courseCode: "BCS2313",
-    courseName: "Artificial Intelligence Techniques",
-    exam: "Y",
-    credit: "3",
-    faculty: "FACULTY OF COMPUTING",
-  },
-];
-
+const chosenCourses = reactive([]);
 const selectedCourses = ref([]);
+const currentPage = ref(1);
+const pageSize = ref(15);
+const totalItems = ref(0);
+const totalPages = computed(() => Math.ceil(totalItems.value / pageSize.value));
+const rowKey = (row) => row.id;
+const courseTable = ref(null);
+
+function handleSelectionChange(val) {
+  selectedCourses.value = val;
+}
+
+function nextPage() {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value++;
+    fetchCourses(currentPage.value);
+  }
+}
+
+function prevPage() {
+  if (currentPage.value > 1) {
+    currentPage.value--;
+    fetchCourses(currentPage.value);
+  }
+}
+
+async function fetchFaculties() {
+  console.log("Fetching faculties...");
+  const { data, error } = await client.from("faculty").select("id, name");
+  if (error) {
+    console.error("Error fetching faculties:", error.message);
+  } else {
+    faculties.value = data;
+    console.log("Faculties fetched:", faculties.value);
+  }
+}
+
+async function fetchCourses(page) {
+  try {
+    const from = (page - 1) * pageSize.value;
+    const to = page * pageSize.value - 1;
+    let query = client
+      .from("course")
+      .select(
+        "id, courseCode, courseName, exam, faculty:faculty_id(name), credit",
+        {
+          count: "exact",
+        }
+      )
+      .range(from, to);
+
+    if (search.courseCode) {
+      query = query.ilike("courseCode", `%${search.courseCode}%`);
+    }
+    if (search.courseName) {
+      query = query.ilike("courseName", `%${search.courseName}%`);
+    }
+    if (search.exam === "Yes") {
+      query = query.eq("exam", "Yes");
+    } else if (search.exam === "No") {
+      query = query.eq("exam", "No");
+    }
+    if (search.faculty) {
+      query = query.eq("faculty_id", search.faculty);
+    }
+    const { data, error, count } = await query;
+
+    if (error) {
+      throw error;
+    }
+
+    coursesData.value = data.map((course) => ({
+      id: course.id,
+      courseCode: course.courseCode,
+      courseName: course.courseName,
+      exam: course.exam,
+      faculty: course.faculty.name,
+      credit: course.credit,
+    }));
+    totalItems.value = count;
+
+    // Maintain the selection state
+    setTimeout(() => {
+      selectedCourses.value.forEach((selectedCourse) => {
+        const course = coursesData.value.find(
+          (course) => course.id === selectedCourse.id
+        );
+        if (course) {
+          courseTable.value.toggleRowSelection(course, true);
+        }
+      });
+    }, 0);
+  } catch (error) {
+    console.error("Error fetching courses:", error.message);
+  }
+}
+
+watchEffect(() => {
+  fetchCourses(currentPage.value);
+});
+
+onMounted(() => {
+  fetchFaculties();
+  fetchCourses(currentPage.value);
+});
 
 definePageMeta({
   layout: "navbar",
